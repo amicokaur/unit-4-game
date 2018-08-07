c7ca9021d706e817d87f867dc267aef5c41a8549
amicokaur <amica0811@yaho..com>
2018-08-06 22:30:25 -0700

    3push

--- assets/game.js
+++ assets/game.js
@@ -1,30 +1,30 @@
 //there are 5 randomly generated numbers.
 //line 2-6 are my global variable 
  var sum= 0
- var image1;
- var image2;
- var image3;
- var image4;
+ var random1;
+ var random2;
+ var random3;
+ var random4;
  var largeNum;
  function startGame(){
-     image1= Math.floor(Math.random()*10)+1
-     image2= Math.floor(Math.random()*10)+1
-     image3= Math.floor(Math.random()*10)+1
-    image4= Math.floor(Math.random()*10)+1
+     random1= Math.floor(Math.random()*10)+1
+     random2= Math.floor(Math.random()*10)+1
+     random3= Math.floor(Math.random()*10)+1
+    random4= Math.floor(Math.random()*10)+1
  largeNum= Math.floor(Math.random()*30)+30 
-$("#img1").data(image1)
+$("#img1").data(random1)
 
-$("#num1").text(image1)
-$("#num2").text(image2)
-$("#num3").text(image3)
-$("#num4").text(image4)
+// $("#num1").text(random1)
+// $("#num2").text(random2)
+// $("#num3").text(random3)
+// $("#num4").text(random4)
 
 $("target").text(largeNum)
 
-$("#num1").attr("value", image1 )
-$("#num2").attr("value", image2 )
-$("#num3").attr("value", image3 )
-$("#num4").attr("value", image4 )
+$("#num1").attr("value", random1 )
+$("#num2").attr("value", random2 )
+$("#num3").attr("value", random3 )
+$("#num4").attr("value", random4 )
 
  }
 $(".button").on('click', function(){
